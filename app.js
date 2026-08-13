76e90bb4485ec1e01b9fa5d7f0ee03ebdb0d1801
joaoltrevisan <jltd17128@gmail.com>
2026-08-13 17:16:14 -0300

    concatenation

--- app.js
+++
@@ -1,2 +0,0 @@
-let score = 100; // cria uma variável chamada score e atribui o valor 100 a ela
-console.log(score); // imprime o valor da variável score no console
---
+++ concat.js
@@ -0,0 +1,13 @@
+let firstName = "Lucas";
+let secondName = "George";
+let fullName = firstName + " " + secondName;
+console.log(fullName); // imprime o nome completo no console
+
+let Age = "I am";
+Age += " 25 years old";
+console.log(Age); // imprime a idade no console
+
+let str1 = '1';
+let str2 = '2'; 
+let result = str1.concat(' '+ str2);
+console.log(result);
