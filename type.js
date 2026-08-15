ecca8b54c44804719e3f3ac65e6dd26e29e43e1a
joaoltrevisan <jltd17128@gmail.com>
2026-08-15 18:27:12 -0300

    gramatic

---
+++ type.js
@@ -0,0 +1,18 @@
+const num = 76;
+console.log(typeof num); // declara o tipo de dado da variável num, que é number
+
+let say = 'Bot';
+console.log(say[1]); // acessa o segundo caractere da string, que é 'o'
+
+const byMe = 'sayless';
+console.log(byMe[byMe.length - 1]); // acessa o último caractere da string, que é 's'
+
+let and = 'The handsome';
+let justSum = and[0] + and[2] + and[3] + and[5] + and [10] + and[9];
+console.log(justSum);  
+
+let phrase = 'I \nlove \nbrunettes'
+console.log(phrase); // imprime a frase com quebras de linha
+
+const buy = 'cunsumer \"needs\" to purchase';
+console.log(buy); // imprime a frase com aspas duplas dentro da string
